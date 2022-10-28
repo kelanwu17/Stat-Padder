d8435c9046c713482e5f424c14d6d127683a5d78
Kelan Wu <90233632+kelanwu17@users.noreply.github.com>
2022-10-28 13:21:13 -0500

    V2
    -changed name from nba-stat-flask... to stat-padder
    -everything is dynamic now
    -2022 rookies show now

--- app.py
+++ app.py
@@ -1,11 +1,91 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import playerawards, commonplayerinfo,franchisehistory, TeamYearByYearStats, commonteamroster, commonteamyears, commonallplayers, playercareerstats
+import json
 
+nba_players = players.get_players()
+
+
+# print(player)
+
+# player_awards = playerawards.PlayerAwards(player_id = player_id)
+# player_json = player_awards.get_normalized_json()
+# player_json_loads = json.loads(player_json)
+# print(json.dumps(player_json_loads, indent = 4))
+# all_defense_team = 0
+# all_nba = 0
+# fmvp = 0
+# mvp = 0
+# roty = 0
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        player_input = request.form.get('player_input')
+        
+        nba_players = json.loads(commonallplayers.CommonAllPlayers().get_normalized_json())
+        player_id = 0;
+        player_name = ""
 
+        for i in range(0, len(nba_players['CommonAllPlayers'])):
+            if nba_players['CommonAllPlayers'][i]['DISPLAY_FIRST_LAST'].lower() == player_input.lower():
+                player_id = nba_players['CommonAllPlayers'][i]['PERSON_ID']
+                player_name = nba_players['CommonAllPlayers'][i]['DISPLAY_FIRST_LAST']
+                break
+        player_image = f'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{player_id}.png'
+        player_info = json.loads((commonplayerinfo.CommonPlayerInfo(player_id = player_id)).get_normalized_json())
+        player_team = player_info["CommonPlayerInfo"][0]["TEAM_NAME"]
+        player_team_id = player_info["CommonPlayerInfo"][0]["TEAM_ID"]
+        player_country = player_info["CommonPlayerInfo"][0]["COUNTRY"]
+        player_height = player_info["CommonPlayerInfo"][0]["HEIGHT"]
+        player_weight = player_info["CommonPlayerInfo"][0]["WEIGHT"]
+        team_image = f'https://cdn.nba.com/logos/nba/{player_team_id}/primary/D/logo.svg'
+        stats = json.loads(playercareerstats.PlayerCareerStats(player_id = player_id).get_normalized_json())
+        careerpts = 0
+        careergames = 0
+        careerast = 0
+        careerreb = 0
+        for i in range(0, len(stats['SeasonTotalsRegularSeason'])):
+            if stats['SeasonTotalsRegularSeason'][i]['GP'] > 0:
+                careergames = careergames + stats['SeasonTotalsRegularSeason'][i]['GP']
+            if stats['SeasonTotalsRegularSeason'][i]['PTS'] > 0:
+                careerpts = careerpts + stats['SeasonTotalsRegularSeason'][i]['PTS']
+            if stats['SeasonTotalsRegularSeason'][i]['REB'] > 0:
+                careerreb = careerreb + stats['SeasonTotalsRegularSeason'][i]['REB']
+            if stats['SeasonTotalsRegularSeason'][i]['AST'] > 0:
+                careerast = careerast + stats['SeasonTotalsRegularSeason'][i]['AST']
+                
+                
+        player_awards = json.loads((playerawards.PlayerAwards(player_id = player_id)).get_normalized_json())        
+        all_d = 0
+        all_nba = 0
+        fmvp = 0
+        mvp = 0
+        roty = 0
+        dpoy = 0
+        mip = 0
+        for i in range(0,len(player_awards["PlayerAwards"])):
+            if (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "All-Defensive Team"):
+                all_d += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "All-NBA"):
+                all_nba += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "NBA Finals Most Valuable Player"):
+                fmvp += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "NBA Most Valuable Player"):
+                mvp += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "NBA Rookie of the Year"):
+                roty += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "NBA Defensive Player of the Year"):
+                dpoy += 1
+            elif (player_awards["PlayerAwards"][i]['DESCRIPTION'] == "NBA Most Improved Player"):
+                mip += 1
+        all_star = 0
+        for i in range(0, len(stats['SeasonTotalsAllStarSeason'])):
+            all_star += 1
+        return render_template("index.html", player_name = player_name, player_image =player_image,  player_country = player_country, career_pts = round(careerpts/careergames,1), career_ast = round(careerast/careergames,1), career_reb =  round(careerreb/careergames,1), all_d = all_d, all_nba = all_nba, fmvp = fmvp, mvp = mvp, roty = roty, dpoy = dpoy, mip = mip, player_weight = player_weight, player_height = player_height, all_star = all_star, team_image = team_image)
+    else:
+        return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
